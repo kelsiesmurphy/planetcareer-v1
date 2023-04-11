6f83735c3995e01e990f7b0f11bef837cd2445d4
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-04-11 12:26:46 +0100

    Putting iframe in own component

--- components/index/Hero.tsx
+++ components/index/Hero.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
 import GetAccessButton from "./GetAccessButton";
 import { ArrowUpRight } from "react-feather";
 import Link from "next/link";
+import IframeDemo from "./IframeDemo";
 
 const Hero = ({ font }: any) => {
   return (
@@ -26,11 +26,7 @@ const Hero = ({ font }: any) => {
         <Link href="/demo" className="btn-secondary"><ArrowUpRight /> View Demo</Link>
         <GetAccessButton styling={"btn-primary"} text="Get Free Access" />
       </div>
-      <iframe
-        src="https://www.planetcareer.co.uk/demo"
-        title="description"
-        className="flex-1 w-full rounded-lg md:rounded-2xl shadow-2xl border-4 md:border-8 border-stone-800 aspect-[1248/916]"
-      ></iframe>
+      <IframeDemo />
     </section>
   );
 };
